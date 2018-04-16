3bc0b113cc1ed3782cb2bcca63ecaec49d0794a3
torychow <522617505@qq.com>
2018-04-17 00:53:10 +0800

    Add web

---
+++ src/main/java/com/job/SkillHunterApplication.java
@@ -0,0 +1,12 @@
+package com.job;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkillHunterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SkillHunterApplication.class, args);
+    }
+}
---
+++ src/main/java/com/job/controller/UserController.java
@@ -0,0 +1,15 @@
+package com.job.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("/user")
+@RestController
+public class UserController {
+
+    @GetMapping
+    public void get(){
+        System.out.println("get");
+    }
+}
